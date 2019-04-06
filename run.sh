816075279a71b6e395b85a4c28a2a93537bcc037
Cass <m@cass.si>
2019-04-06 23:07:38 +0100

    add script

---
+++ run.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+client=0
+server=0
+
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -s|--server) server=1; shift;;
+  -c|--client) client=1;;
+  *) echo "Unknown parameter passed: $1"; exit 1;;
+esac; shift; done
+
+if [ "$server" -eq 1 ]; then
+  cd Server/
+  moonc -t compiled/ .
+  love compiled/
+elif [ "$client" -eq 1 ]; then
+  cd Client/
+  moonc -t compiled/ .
+  love compiled/
+else
+  exit 0
+fi
